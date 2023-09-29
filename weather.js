475c8e2cc2d5651de7f201def8bd1172d681f4a3
Yogita Ghodake <yogitaghodake93@gmail.com>
2023-09-29 14:21:15 +0530

    Initial commit

---
+++ weather.js
@@ -0,0 +1,78 @@
+const unitSelector = document.getElementById("unitSelector");
+const searchButton = document.querySelector(".search button");
+const searchBar = document.querySelector(".search-bar");
+
+
+let selectedUnit = "celsius";
+
+unitSelector.addEventListener("change", function () {
+    selectedUnit = unitSelector.value;
+    weather.fetchWeather(document.querySelector(".city").innerText);
+  });
+
+let weather = {
+    "apiKey":"8f655e4f495699c2293a36edadf850e5",
+    lastCity: "",
+    fetchWeather: function (city) {
+        this.lastCity = city;
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" 
+        + city 
+        + "&units=metric&appid=" 
+        + this.apiKey         
+        ).then((response) => {
+            if (! response.ok){
+                alert("No weather found.");
+                throw new Error("No weather found.");
+            }
+            return response.json();
+        })
+        .then((data) => this.displayWeather(data));
+    },
+
+
+
+    displayWeather: function(data){
+        const{ name } = data;
+        const{ icon,description} = data.weather[0];
+        const{ temp,humidity, speed}  = data.main;
+        // const{ speed } = data.wind;
+        let convertedTemp = temp;
+
+        const unit = selectedUnit.toLowerCase();
+
+        if (unit === 'fahrenheit'){
+            convertedTemp  = (temp * 9/5) + 32;
+        }
+
+        console.log(name,icon,description,convertedTemp,humidity,speed);
+        document.querySelector(".city").innerText = "Weather in "+ name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+ icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = convertedTemp.toFixed(2) + "°" + (unit === "fahrenheit" ? "F" : "C");
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/h";
+        document.querySelector(".weather").classList.remove("loading");
+
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + " ')";
+    },
+
+    search: function(){
+        const city = searchBar.value;
+        if(city !== ""){
+            this.fetchWeather(city);
+        }
+    },
+};
+
+searchButton.addEventListener("click", function(){
+    weather.search();
+});
+
+searchBar.addEventListener("keyup", function(event)
+{
+    if(event.key == "Enter"){
+        weather.search();
+    }
+});
+
+weather.fetchWeather("Denver");
